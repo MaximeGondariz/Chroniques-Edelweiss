89fe47f626cdc7eee651cf040923d469a12f5de4
MaximeGondariz <maxime.gondariz2909@gmail.com>
2023-10-26 13:32:34 +0200

    Ajout des records

--- chroniques-edelweiss-front/src/app/record/record.component.ts
+++ chroniques-edelweiss-front/src/app/record/record.component.ts
@@ -9,68 +9,59 @@ export class RecordComponent {
   recordTable= [{
     name: 'Primevère',
     record: [{
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Nombre de mort lors de la première séance d\'un personnage',
+      players: ['Windfast'],
+      highscore: '2 morts'
     },
     {
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Durée de survie la plus courte',
+      players: ['Windfast'],
+      highscore: '2 heures'
     },
     {
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Nombre de mort d\'un même personnage',
+      players: ['Noenra'],
+      highscore: '2 morts'
+    },
+    {
+      title: 'Fuite avec la plus grande durée',
+      players: ['SMPK10', 'William Alpha'],
+      highscore: '16 jours'
     }]
   },
   {
-    name: 'Donjon Explorer',
+    name: 'Tournoi d\'Edelweiss',
     record: [{
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Nombre de fois où le titre de Champion a été obtenu',
+      players: ['SMPK'],
+      highscore: '2 fois'
     },
     {
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Nombre d\'année consécutive en ayant le titre',
+      players: ['SMPK'],
+      highscore: '2 années consécutives'
     },
     {
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Défaite la plus courte',
+      players: ['Windfast'],
+      highscore: '5 minutes'
     }]
   },
   {
     name: 'Pokemon',
     record: [{
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'La plus grosse collection de pokémon',
+      players: ['William Alpha'],
+      highscore: '14 pokemons'
     },
     {
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
-    },
-    {
-      title: 'LOREM IPSUM',
-      players: [{
-        pseudo: 'LOREM'
-      }]
+      title: 'Celui qui est le plus tombé KO lors d\'une séance',
+      players: ['William Alpha'],
+      highscore: '4 KO'
     }]
   }]
 
-  recordShown: number | undefined;
+  recordShown = 1;
 
   constructor(){}
 
